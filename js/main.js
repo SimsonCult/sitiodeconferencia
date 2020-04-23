e326246af165bcf5a3fbce5a4c512c6e0f1a5666
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-23 05:50:26 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,20 @@
     var regalo = document.getElementById("regalo");
     document.addEventListener('DOMContentLoaded', function() {
 
+
+
+        var map = L.map('mapa').setView([-51.634147, -69.203369], 16);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([-51.634147, -69.203369]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+
+
+
         //campos datos usuarios
         var nombre = document.getElementById("nombre");
         var apellido = document.getElementById("apellido");
