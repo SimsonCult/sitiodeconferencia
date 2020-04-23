d14caa0ed765f7e8820d50db229ba39885a2e180
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-23 18:52:11 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(function() {
 
-    $('div.ocultar:first').show();
 
+    $('div.ocultar:first').fadeIn();
+    $('.menu-programa a:first').addClass('activo');
 
     $('.menu-programa a').on('click', function() {
 
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass("activo");
+
         $('.ocultar').hide();
         var enlace = $(this).attr('href');
-        $(enlace).show();
+        $(enlace).fadeIn();
 
 
         return false;
