d43c01079205b522ea2aac2df745cc5d207546ea
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-23 02:23:04 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,22 @@
     'use strict';
     document.addEventListener('DOMContentLoaded', function() {
 
+        //campos datos usuarios
+        var nombre = document.getElementById("nombre");
+        var apellido = document.getElementById("apellido");
+        var email = document.getElementById("email");
 
+        //campo pases
+        var paseDia = document.getElementById("paseDia");
+        var pase2Dias = document.getElementById("pase2Dias");
+        var paseCompleto = document.getElementById("paseCompleto");
 
-        alert("hola");
+        //botones y divs
+
+        var calcular = document.getElementById("calcular");
+        var errorDiv = document.getElementById("error");
+        var botonRegisto = document.getElementById("btnRegistro");
+        var resultado = document.getElementById("listaProductos");
 
 
     });
