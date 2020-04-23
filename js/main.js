1c9c09051616eafe61d16760bd8bd938aa09a9e5
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-23 19:23:22 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,4 +23,20 @@ $(function() {
     $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15 }, 1200);
     $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 1500);
     $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9 }, 1200);
+
+
+
+    //contador de cuenta regresiva
+
+    $('.cuenta-regresiva').countdown('2020/08/27 00:00:00', function(event) {
+
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+
+
+
+    });
+
 });
