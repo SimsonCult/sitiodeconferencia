5ca92bed55d25fedd106036106191d34d948c521
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-26 21:08:09 -0300

    Create conferencia.php

---
+++ conferencia.php
@@ -0,0 +1,47 @@
+<?php include_once 'includes\templates\header.php'; ?>
+
+
+    <section class="seccion contenedor">
+        <h2>La mejor Conferencia de Diseño Web en Español</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus corporis eius earum tempora excepturi, incidunt aspernatur molestias, tempore accusamus sequi iusto dignissimos. Doloribus maiores similique dolore voluptatibus blanditiis sequi
+            pariatur. eveniet placeat architecto omnis voluptatum saepe eaque, officiis excepturi alias vel doloremque est, quod iusto quis dolore debitis hic ea. </p>
+    </section>
+
+
+    <section class="seccion contenedor">
+        <h2>galeria de fotos</h2>
+        <div class="galeria">
+            <a href="img/galeria/01.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/01.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/02.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/02.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/03.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/03.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/04.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/04.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/05.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/05.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/06.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/06.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/07.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/07.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/08.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/08.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/09.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/09.jpg" alt="imagen invitado">
+            </a>
+            <a href="img/galeria/10.jpg" data-lightbox='galeria'>
+                <img src="img/galeria/thumbs/10.jpg" alt="imagen invitado">
+            </a>
+        </div>
+    </section>
+
+    <?php include_once 'includes\templates\footer.php'; ?>
