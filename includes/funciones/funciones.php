7a0fad9b67942a9c3e7faf4f35efa9c004b8f7a1
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-26 21:08:32 -0300

    Create funciones.php

---
+++ includes/funciones/funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+function productos_json (&$boletos, &$camisas = 0, &$etiquetas = 0){
+    $dias = array(0=> 'un_dia',1=> 'pase_completo',2=> 'pase_2dia');
+
+    echo '<pre>';
+        var_dump($dias);
+    echo '</pre>';
+
+}
+
+
+
+?>
