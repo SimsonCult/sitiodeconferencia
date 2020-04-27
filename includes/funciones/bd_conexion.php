68b9cd916a2bed2ad14859bbd2814e6bea4f04e7
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-04-26 21:08:29 -0300

    Create bd_conexion.php

---
+++ includes/funciones/bd_conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = new mysqli('localhost','root','root','gdlwebcamp');
+
+
+if($conn->connect_error) {
+    echo $error = $conn->connect_error; 
+}
+
+if(!mysqli_set_charset($conn, 'utf8')) {
+    printf('Error cargando los caracteres', mysqli_error($conn)); 
+    exit(); 
+}
+
+?>
